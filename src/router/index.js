40afb5179953af7f9f047e6ffe12ff76bdc00c00
Guilherme Silva <guilherme.silva@codeblox.com.br>
2020-11-05 11:56:11 -0300

    Criando projeto e adicionando firebase

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Login from '../views/Login.vue'
+import Signup from '../views/SignUp.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
